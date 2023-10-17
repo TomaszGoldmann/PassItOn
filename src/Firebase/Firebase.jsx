fa1244ac918871a53a417f843e8abf2651e5a1c7
Tomasz Goldmann <tgoldmann3b1@gmail.com>
2023-10-17 14:52:41 +0200

    Deploy

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
+    apiKey: "AIzaSyAtbwDczkYFDUWoEdqeOH_LR",
     authDomain: "passiton-6b6e6.firebaseapp.com",
     projectId: "passiton-6b6e6",
     storageBucket: "passiton-6b6e6.appspot.com",
     messagingSenderId: "237406923238",
     appId: "1:237406923238:web:20ea2e920b870fb11ece27"
 };
+
+console.log(import.meta.env.VITE_API_KEY)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
