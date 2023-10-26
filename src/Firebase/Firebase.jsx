5e665bf79c3fa37088ea35096325cfa27b9262c9
Tomasz Goldmann <tgoldmann3b1@gmail.com>
2023-10-26 18:24:46 +0200

    API KEY

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -4,8 +4,8 @@ import {getFirestore, collection} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAtbwDczkYFDUWoEdqeOH_LR-_S7-F6ID4",
-    // apiKey: import.meta.env.VITE_API_KEY,
+    // apiKey: "AIzaSyAtbwDczkYFDUWoEdqeOH_LR-_S7-F6ID4",
+    apiKey: import.meta.env.VITE_API_KEY,
     authDomain: "passiton-6b6e6.firebaseapp.com",
     projectId: "passiton-6b6e6",
     storageBucket: "passiton-6b6e6.appspot.com",
